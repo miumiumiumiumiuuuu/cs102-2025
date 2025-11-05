df19150b867cde4cf3090537574cb050a6699775
frolova elizaveta@ <miiiumaaaulalala@gmail.com>
2025-11-05 14:56:51 +0300

    Реализована функция encrypt_caesar()

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -48,6 +48,7 @@ def multiplicative_inverse(e: int, phi: int) -> int:
     >>> multiplicative_inverse(7, 40)
     23
     """
+
     def extended_gcd(a: int, b: int) -> tp.Tuple[int, int, int]:
         if a == 0:
             return b, 0, 1
@@ -104,7 +105,7 @@ def decrypt(pk: tp.Tuple[int, int], ciphertext: tp.List[int]) -> str:
     # Unpack the key into its components
     key, n = pk
     # Generate the plaintext based on the ciphertext and key using a^b mod m
-    plain = [chr((char ** key) % n) for char in ciphertext]
+    plain = [chr((char**key) % n) for char in ciphertext]
     # Return the array of bytes as a string
     return "".join(plain)
 
